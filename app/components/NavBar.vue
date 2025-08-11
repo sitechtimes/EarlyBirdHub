64d6b2a3345630436edb8c72ae4792b7251cd577
joe-238 <144480253+jo-svg@users.noreply.github.com>
2025-08-11 18:20:21 -0400

    changed nav to x

--- app/components/NavBar.vue
+++ app/components/NavBar.vue
@@ -38,54 +38,26 @@
   </nav>
   <div
     class="absolute top-4 left-0 z-50 flex items-center justify-center cursor-pointer"
-    @click="toggleNavbar"
+    @click="isOpen = !isOpen"
   >
     <button
-      class="p-2 rounded transition duration-150"
-      :class="{ spin: isOpening, spinOut: isClosing }"
-      aria-label="Open Navbar"
+      class="p-2 flex flex-col rounded transition duration-150"
+      aria-label="Toggle Navbar"
     >
-      <span class="block w-7 h-1 bg-white mb-1"></span>
-      <span class="block w-7 h-1 bg-white mb-1"></span>
-      <span class="block w-7 h-1 bg-white"></span>
+      <span
+        class="block w-8 h-1 bg-white rounded-sm mb-2 transform transition-all duration-300 origin-center"
+        :class="isOpen ? 'rotate-45 translate-y-1.5' : 'rotate-0 '"
+      ></span>
+      <span
+        class="block w-8 h-1 bg-white rounded-sm transform transition-all duration-300 origin-center"
+        :class="isOpen ? '-rotate-45 -translate-y-1.5' : 'rotate-0'"
+      ></span>
     </button>
     <p class="text-white font-bold" v-if="windowWidth > 600">Menu</p>
   </div>
 </template>
 
 <script setup>
-const router = useRouter();
-
-const navbarStore = useNavbarStore();
+const isOpen = ref(false);
 const { windowWidth } = useWindowSize();
-
-const isOpening = ref(false);
-const isClosing = ref(false);
-const isOpen = computed(() => navbarStore.isOpen);
-
-function toggleNavbar() {
-  if (isOpen.value) {
-    isClosing.value = true;
-
-    setTimeout(() => {
-      navbarStore.toggleNavbar();
-      isClosing.value = false;
-    }, 300);
-  } else {
-    isOpening.value = true;
-    navbarStore.toggleNavbar();
-    setTimeout(() => {
-      isOpening.value = false;
-    }, 300);
-  }
-}
-
-watch(
-  () => router.currentRoute.value,
-  () => {
-    if (isOpen.value) {
-      toggleNavbar();
-    }
-  }
-);
 </script>
